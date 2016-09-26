eedfc8d023197d55545447b04f6975321b07b3e9
thinh nguyen <thinhnguyen@thinhs-MacBook-Air.local>
2016-09-25 23:20:24 -0700

    question 2

---
+++ launchschool-lesson-3/easy1-Q-2.rb
@@ -0,0 +1,9 @@
+# 1, != mean not equal
+
+# 2, !<name> mean not name or opposite of name
+
+# 3, <name>! will alter name
+
+# 4, ? mean to return yes or no or true or false 
+
+# 5, !!name = name => false but !name = name => return true
