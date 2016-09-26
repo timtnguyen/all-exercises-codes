fd121d0ffc34b5529e26a15aa9ce55cb6658daf4
thinh nguyen <thinhnguyen@thinhs-MacBook-Air.local>
2016-09-26 10:37:05 -0700

    question 4

---
+++ launchschool-lesson-3/easy1-Q-4.rb
@@ -0,0 +1,9 @@
+numbers = [1, 2, 3, 4, 5]
+
+numbers.delete_at(1)
+puts numbers
+# This is to delete index[1] in the array
+
+numbers.delete(1)
+puts numbers
+# This is to delete number 1 in the array
