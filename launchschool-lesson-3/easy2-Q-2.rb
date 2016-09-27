2b4686fe739a2ce2f447832ed211e26fd980b706
thinh nguyen <thinhnguyen@thinhs-MacBook-Air.local>
2016-09-26 22:11:29 -0700

    question 2 - easy-2

---
+++ launchschool-lesson-3/easy2-Q-2.rb
@@ -0,0 +1,9 @@
+ages = {
+  "Herman"   => 32,
+  "Lily"     => 30,
+  "Grandpa"  => 5843,
+  "Eddie"    => 10,
+  "Marilyn"  => 22,
+  "Spot"     => 237
+}
+ puts ages.inject(0) { |total, age| total += age[1]}
