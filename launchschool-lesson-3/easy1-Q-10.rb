672706cd9e1797d672fb70ce8d8b6fdeb98a84e3
thinh nguyen <thinhnguyen@thinhs-MacBook-Air.local>
2016-09-26 20:28:24 -0700

    question 10

---
+++ launchschool-lesson-3/easy1-Q-10.rb
@@ -0,0 +1,7 @@
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "Pebbles", "BamBam"]
+
+flintstones_hash = {}
+flintstones.each_with_index do |name, index|
+   flintstones_hash[name] = index
+   puts flintstones_hash 
+end
