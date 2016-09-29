bf2cd9afd47dffbeb5db055daa6502b6592120ad
thinh nguyen <thinhnguyen@thinhs-MacBook-Air.local>
2016-09-28 21:51:19 -0700

    question 5 - medium 1

---
+++ launchschool-lesson-3/medium1-Q-5.rb
@@ -0,0 +1,11 @@
+def factors(number)
+  dividend = number
+  divisors = []
+  begin
+    divisors << number / dividend if number % dividend == 0
+    dividend -= 1
+  end until dividend == 0
+  divisors
+end
+
+p factors(10)
