fa1c16102bc3e8a6a70143b0a408ee529051e5af
thinh nguyen <thinhnguyen@thinhs-MacBook-Air.local>
2016-09-28 22:19:07 -0700

    question 5 - medium 1

--- launchschool-lesson-3/medium1-Q-5.rb
+++ launchschool-lesson-3/medium1-Q-5.rb
@@ -1,11 +1,14 @@
 def factors(number)
   dividend = number
   divisors = []
-  begin
+while dividend > 0 do
     divisors << number / dividend if number % dividend == 0
     dividend -= 1
-  end until dividend == 0
-  divisors
 end
-
+divisors
+end
 p factors(10)
+
+# 1, Make sure the number with no remainder
+
+# 2, return the value divisors 
