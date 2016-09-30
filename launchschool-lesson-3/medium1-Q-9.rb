68aed51cdec16d8ad2d6800c0d6ae6185978ff6c
thinh nguyen <thinhnguyen@thinhs-MacBook-Air.local>
2016-09-29 21:37:36 -0700

    question 9 - medium 1

--- launchschool-lesson-3/medium1-Q-9.rb
+++ launchschool-lesson-3/medium1-Q-9.rb
@@ -1,10 +1,29 @@
 munsters = {
-  "Herman"  => { "age" => 32, "gender" => "male"},
-  #{}"Lily"    => { "age" => 30, "gender" => "female"},
-  #{}"Grandpa" => { "age" => 402,"gender" => "male"},
-  #{}"Eddie"   => { "age" => 10, "gender" => "male"},
-  #{}"Marilyn" => { "age" => 23, "gender" => "female"}
+  "Herman"   => { "age" => 32, "gender" => "male"},
+  "Lily"     => { "age" => 30, "gender" => "female"},
+  "Grandpa"  => { "age" => 402,"gender" => "male"},
+  "Eddie"    => { "age" => 10, "gender" => "male"},
+  "Marilyn"  => { "age" => 23, "gender" => "female"}
 }
-new_munsters = Hash.new { |hsh,key| hsh[key] = {} }
-new_munsters["Herman"].store "age_range", "junior"
-puts new_munsters
+
+munsters["Herman"]["age_group"] = "adult"
+munsters["Lily"]["age_group"] = "adult"
+munsters["Grandpa"]["age_group"] = "senior"
+munsters["Eddie"]["age_group"] = "kid"
+munsters["Marilyn"]["age_group"] = "adult"
+
+puts munsters
+
+or
+
+munsters.each do |name, detail|
+  case detail["age"]
+  when 0..18
+    detail["age_group"] = "kid"
+  when 18..65
+    detail["age_group"] = "adult"
+  else
+    detail["age_group"] = "senior"
+  end
+end
+puts munsters
