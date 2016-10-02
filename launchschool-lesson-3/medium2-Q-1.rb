a53dbafe81bbf7242936f623edef6d91dd3640af
thinh nguyen <thinhnguyen@thinhs-MacBook-Air.local>
2016-10-01 22:08:19 -0700

    question 1

--- launchschool-lesson-3/medium2-Q-1.rb
+++ launchschool-lesson-3/medium2-Q-1.rb
@@ -2,11 +2,11 @@ munsters = {
   "Herman"  => { "age" => 32,  "gender" => "male"},
   "Lily"    => { "age" => 30,  "gender" => "female"},
   "Grandpa" => { "age" => 403, "gender" => "male"},
-  "Eddie"   => { "age" => 10,  "gender" => "male"}
+  "Eddies"   => { "age" => 10,  "gender" => "male"}
 }
 total_age = 0
 munsters.each do |name, detail|
   if detail["gender"] == "male"
     puts total_age += detail["age"]
-  end 
+  end
 end
