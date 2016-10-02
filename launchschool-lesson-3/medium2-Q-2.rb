e84f397dff067e908375ad2d0de69c1d6aa3910d
thinh nguyen <thinhnguyen@thinhs-MacBook-Air.local>
2016-10-01 22:10:12 -0700

    question 2

--- launchschool-lesson-3/medium2-Q-2.rb
+++ launchschool-lesson-3/medium2-Q-2.rb
@@ -1,7 +1,7 @@
 munsters = {
   "Herman"  => { "age" => 32,  "gender" => "male"},
   "Lily"    => { "age" => 30,  "gender" => "female"},
-  "Grandpa" => { "age" => 403, "gender" => "male"},
+  "Grandpas" => { "age" => 403, "gender" => "male"},
   "Eddie"   => { "age" => 10,  "gender" => "male"},
   "Marilyn" => { "age" => 23,  "gender" => "female"}
 }
