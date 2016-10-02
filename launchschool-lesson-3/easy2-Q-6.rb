c14dc0064f9d15743c07e521079bab4ece947fcd
thinh nguyen <thinhnguyen@thinhs-MacBook-Air.local>
2016-10-01 21:31:48 -0700

    question 6

--- launchschool-lesson-3/easy2-Q-6.rb
+++ launchschool-lesson-3/easy2-Q-6.rb
@@ -4,9 +4,9 @@ ages = {
   "Grandpa"  => 5843,
   "Eddie"    => 10,
   "Marilyn"  => 22,
-  "Spot"     => 237
+  "Spots"     => 237
 }
 
 #puts ages.group_by { |k, v| v}.min_by { |k, v| k}.last.to_h
 
-puts ages.group_by(&:last).min_by(&:first).last.to_h 
+puts ages.group_by(&:last).min_by(&:first).last.to_h
