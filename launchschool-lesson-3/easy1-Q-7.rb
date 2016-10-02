f5d73f6b2f93d76b5387248d74482e1cf3575904
thinh nguyen <thinhnguyen@thinhs-MacBook-Air.local>
2016-10-01 21:09:00 -0700

    question 7

--- launchschool-lesson-3/easy1-Q-7.rb
+++ launchschool-lesson-3/easy1-Q-7.rb
@@ -8,7 +8,7 @@ how_deep = "number"
 5.times {how_deep.gsub!("number", "add_eight(number)")}
 
 p how_deep
-#==> returned "add_eight(add_eight(add_eight(add_eight(add_eight(number)))))"
+# => returned "add_eight(add_eight(add_eight(add_eight(add_eight(number)))))"
 
 p eval(how_deep)
  #==> 42
